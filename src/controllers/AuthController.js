527926852c871d302a1420d29fce6222f887663a
Vicman2 <vicmanthebest@gmail.com>
2019-11-02 14:06:42 +0100

    Some changes

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -83,6 +83,8 @@ module.exports = function authController() {
         userService.getFollowers(comedianId)
         .then(data => res.status(200).json(data))
         .catch(err => res.status(500).json(err))
+
+        //  Trying out something
     }
 }
 
